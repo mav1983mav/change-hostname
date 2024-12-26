fa285a2ffe8ec307a267744361c96c1bdb7d3c5f
mav <mav1983@mail.ru>
2024-12-26 07:15:05 +0300

    finish script

--- change-hostname.sh
+++ change-hostname.sh
@@ -1,50 +1,47 @@
 #!/bin/bash
 #
 #supported by Alt Server 10.2
-#change hostname (name computer) in the three place
+#change hostname (name computer) in the three file
 	#/etc/hostname (if is not empty)
 	#/etc/sysconfig/network
 	#/proc/sys/kernel/hostname
+#after rename hostname, you need reload PC     shutdown -r now
 
-place1="/etc/hostname"
-place2="/etc/sysconfig/network"
-place3="/proc/sys/kernel/hostname"
 
-nametest=$(cat $place1)
+file1="/etc/hostname"
+file2="/etc/sysconfig/network"
+file3="/proc/sys/kernel/hostname"
+
+curhostname=$HOSTNAME
 newhostname=$1
-if [ $nametest == $newhostname ]; then
+if [ $curhostname == $newhostname ]; then
 	echo "Это имя компьютера уже установлено"
 	else
 
 
-	if [ -f $place1 ]; then
-		echo "Файл $place1 есть, изменяю имя компьютера: $newhostname"
-		echo $newhostname > $place1
+	if [ -f $file1 ]; then
+		echo "Файл $file1 есть, изменяю имя компьютера на: $newhostname"
+		echo $newhostname > $file1
 	else
-		echo "Файла $place1 нет, создаю файл и задаю новое имя компьютера: $newhostname"
-		touch $place1 && chmod 644 $place1 && echo $newhostname > $place1
+		echo "Файла $file1 нет" #, создаю файл и задаю новое имя компьютера: $newhostname"
+		#touch $file1 && chmod 644 $file1 && echo $newhostname > $file1
 	fi
 
-        if [ -f $place2 ]; then
-		namesearch="HOSTNAME=$nametest"
-                echo "Файл $place2 есть, изменяю имя компьютера: $newhostname"
-                sed -i "s/$namesearch/HOSTNAME=$newhostname/" $place2
+        if [ -f $file2 ]; then
+		namesearch="HOSTNAME=$curhostname"
+                echo "Файл $file2 есть, изменяю имя компьютера на: $newhostname"
+                sed -i "s/$namesearch/HOSTNAME=$newhostname/" $file2
         else
-                echo "Файла $place2 нет, создаю файл и задаю новое имя компьютера: $newhostname"
-                #touch $place2 && chmod 644 $place2 && echo $newhostname > $place2
+                echo "Файла $file2 нет" #, создаю файл и задаю новое имя компьютера: $newhostname"
+                #touch $file2 && chmod 644 $file2 && echo $newhostname > $file2
         fi
 
-
-        if [ -f $place3 ]; then
-                echo "Файл $place3 есть, изменяю имя компьютера: $newhostname"
-                echo $newhostname > $place3
+        if [ -f $file3 ]; then
+                echo "Файл $file3 есть, изменяю имя компьютера на: $newhostname"
+                echo $newhostname > $file3
         else
-                echo "Файла $place3 нет, создаю файл и задаю новое имя компьютера: $newhostname"
-                touch $place3 && chmod 644 $place3 && echo $newhostname > $place3
+                echo "Файла $file3 нет" #, создаю файл и задаю новое имя компьютера: $newhostname"
+                #touch $file3 && chmod 644 $file3 && echo $newhostname > $file3
         fi
-
 
 fi
-place11=""
-place21=""
-place31=""
